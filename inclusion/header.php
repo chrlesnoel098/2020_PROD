3a0eb32bc79033fb002a84990b6dfee1fd5c44fc
Alejandra Miranda <ale.miranda890@gmail.com>
2020-10-25 00:34:33 -0400

    fixing some issues with fontawesome

--- inclusion/header.php
+++ inclusion/header.php
@@ -7,7 +7,6 @@
     <meta name="description" content="TIM - Techinque d'Intégration multimédia collège Maisonneuve">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/normalize.css">
-
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
@@ -19,16 +18,15 @@
 		</div>
 
 		<nav id="site-navigation" class="main-navigation">
+        
             <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">Primary Menu</button>
             <div class="menu-menu-1-container">
                 <ul id="primary-menu" class="menu nav-menu">
                     <li><a href="" aria-current="page">Accueil</a></li>
-                    <i class="fad fa-bars"></i>
                     <li><a href="#SectionProgramme">Programme</a></li>
                     <li><a href="#SectionProfesseur">Professeur</a></li>
                     <li><a href="#SectionProjet">Projet</a></li>
                     <li><a href="#SectionStage">Stages</a></li>
-                    
                     <li><a href="#SectionEvenement">Évènement</a></li>
                     <li><a target="_blank" href="https://www.facebook.com/maisonneuvetim/"><img src="images/icone-facebook.png" alt=""></a></li>
                     <li><a target="_blank" href="https://www.instagram.com/maisonneuvetim/"><img src="images/icone-instagram.png" alt=""></a></li>
