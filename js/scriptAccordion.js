4af42c32ab7f7ea100c38c9c999e8e26fe4cf28e
Lauryan <lauryan-gervais@hotmail.com>
2020-10-28 16:21:29 -0400

    Toggle l'affichage des session accordion.js

--- js/scriptAccordion.js
+++ js/scriptAccordion.js
@@ -1,14 +1,7 @@
-var acc = document.getElementsByClassName("boutonAfficheFenetre");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        /* Toggle between hiding and showing the active panel */
-        var fenetre = document.getElementsByClassName("fenetreCours");
-        if (fenetre.style.display === "flex") {
-            fenetre.style.display = "none";
-        } else {
-            fenetre.style.display = "flex";
-        }
-  });
+// Toggle l'affichage de tous les cours d'une session
+function afficherCours() {
+    console.log("Allo");
+    let fenetreDescriptionCours = document.querySelector("div.fenetreCours");
+    fenetreDescriptionCours.classList.toggle("cachee");
 }
+
