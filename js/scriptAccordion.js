43986829654f29aedf134690c8785e8f9693bfe9
Lauryan <lauryan-gervais@hotmail.com>
2020-10-28 17:26:59 -0400

    Ajout de gitignore et suppression de styles.css et map.css

--- js/scriptAccordion.js
+++ js/scriptAccordion.js
@@ -1,7 +1,17 @@
 // Toggle l'affichage de tous les cours d'une session
-function afficherCours() {
+function afficherLesCours() {
     console.log("Allo");
-    let fenetreDescriptionCours = document.querySelector("div.fenetreCours");
-    fenetreDescriptionCours.classList.toggle("cachee");
+    let fenetreDesCours = document.querySelector("div.fenetreCours");
+    fenetreDesCours.classList.toggle("cachee");
 }
 
+// Toggle l'affichage de la description d'un cours
+let fenetreDescriptionCours = document.getElementsByClassName("h3.titreCours");
+fenetreDescriptionCours.addEventListener("click", afficherUnCours);
+
+
+function afficherUnCours() {
+    console.log("Allo");
+    let descriptionCours = document.querySelector("section.descriptionCours ul");
+    descriptionCours.classList.toggle("cachee");
+}
