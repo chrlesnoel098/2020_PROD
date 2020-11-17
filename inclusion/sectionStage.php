dfdca6f411dd3617dd24d0a641a7fe83b557de90
Alejandra Miranda <ale.miranda890@gmail.com>
2020-11-16 20:41:37 -0500

    css + html modif

--- inclusion/sectionStage.php
+++ inclusion/sectionStage.php
@@ -1,41 +1,41 @@
         <!-- Début de la section Stage -->
         <section class="stage" id="Stage">
-           <!-- <h1 id="SectionStage">Stages</h1> -->
-          <div class="Carrousel_container">
-                <p>stylos para el carrousel</p>
-                <div class="fleches">hola</div>
-                <div class="slide1">hola</div>
-                <div class="slide2">hola</div>
-                <div class="slide3">hola</div>
-                <div class="points">hola</div>
-            </div> 
+            <div class="stagecontent">
 
-      
-            <div class="stage_container">
-                <div class="stage-info"><!-- bloque NEGROS -->
-
-                    <!-- primer bloque -->
-                    <div class="stagier">
-                        <div class="stage-photo">
-                            <img class="stage-photo" src="images/profs/camille.jpg" alt="photo du stagier">
-                        </div>
-                        
-                        <div class="textos">
-                            <p>Nombre étudiant</p>
-                            <p>Type de stage</p>
-                        </div>
-                    <div>
-                    
-                    
-                    <div class="stage-temoignage">
-                        <h3>Témoigne</h3>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
-                    </div>  
-                </div>
-            </div>
             
+                <div class="CarrouselContent">
+                    <div class="carrouselPhotos">
+                        <p>stylos para el carrousel</p>
+                        <div class="fleches">hola</div>
+                        <div class="slide1">hola</div>
+                        <div class="slide2">hola</div>
+                        <div class="slide3">hola</div>
+                        <div class="points">hola</div>
+                    </div>
+                </div> 
+
+                
            
+                <div class="stagierContent">
+                    <div class="stagier-info">
+                        <div class="stagier">
+                                <img class="stagierPhoto" src="images/profs/camille.jpg" alt="photo du stagier">
+                                <p>Nombre étudiant</p>
+                                <p>Type de stage</p>
+                        <div>
+                        <div class="stage-temoignage">
+                            <h3>Témoigne</h3>
+                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+                        </div>  
 
+                        <!--  -->
+                        
+                    </div>
+                </div>
+            
+                
 
+            </div>
         </section>
         <!-- Fin de la section Stage -->
