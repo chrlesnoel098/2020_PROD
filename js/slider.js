f8152f4fbe84f618125606066f4393e9035b9179
Alejandra Miranda <ale.miranda890@gmail.com>
2020-11-17 19:08:53 -0500

    html + css + js

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,20 @@
 
 
-  //  alert('hi');
+  // alert('hi');
     /* var propSlider = {
         slider: document.getElementById('SliderCarrousel'),
         primerSlide: null
     }
 
     var methodeSlider = {
-        inicio: function () {
+        inicio2: function () {
             setInterval(methodeSlider.move, 3000);
         },
 
         move: function () {
             propSlider.slider.style.transition = 'all 1s ease';
             propSlider.slider.style.marginLeft = '-100%';
+            //console.log(propSlider.slider);
 
             setTimeout(function () {
                 propSlider.primerSlide = propSlider.slider.firstElementChild;
@@ -24,8 +25,9 @@
         }
     }
 
-    methodeSlider.inicio();
- */
+    
+    methodeSlider.inicio2(); */
+
 
        
     
