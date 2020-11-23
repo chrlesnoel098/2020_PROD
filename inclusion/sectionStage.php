888ec2715f4ba99ae29e68bc78718bc916079a23
Alejandra Miranda <ale.miranda890@gmail.com>
2020-11-22 22:10:57 -0500

    section stage terminée

--- inclusion/sectionStage.php
+++ inclusion/sectionStage.php
@@ -1,8 +1,74 @@
 <!-- Début de la section Stage -->
 <section class="stage" id="Stage">
     <h1 id="SectionStage">Stages</h1>
-    <div class="stagecontent">
 
+    <!-------------------------------------------------------- Carrousel container----->
+    <div class="stageCarrousel-container">
+        <div class="mySlides fade">
+            <img src="images/Stagiers/stages1.jpg"/>
+            <div class="text">Caption Text</div>
+        </div>
+
+        <div class="mySlides fade">
+            <img src="images/Stagiers/stages2.jpg"/>
+            <div class="text">Caption Two</div>
+        </div>
+
+        <div class="mySlides fade">
+            <img src="images/Stagiers/stages3.jpg"/>
+            <div class="text">Caption Three</div>
+        </div>
+
+        <!-- buttons avant et après -->
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    </div>
+    <br>
+    <!-- fin du conteneur carrousel -->
+
+    <!-- les petits circles -->
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+    <!---------------------------------------------------------Fin carrousel -->
+
+    <!-------------------------------------------------------Expériences de stage -->
+    <div class="petitmot">
+        <p>Stages au milieu de travail</p>
+        <hr>
+        <p>Expériences</p>
+    </div>
+
+    <div class="stagierContent">
+            <div class="stagier-info">
+                <div class="stagier">
+                    <img class="stagierPhoto" src="images/Stagiers/avatar1.JPG" alt="photo du stagier">
+                    <p>Nom étudiante</p>
+                    <p>Type de stage</p>
+                </div>
+                <div class="stage-temoignage">
+                    <h3>Témoigne</h3>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+                </div>  
+            </div>
+           
+            <div class="stagier-info">
+                <div class="stagier">
+                    <img class="stagierPhoto" src="images/Stagiers/avatar2.JPG" alt="photo du stagier">
+                    <p>Nom étudiant</p>
+                    <p>Type de stage</p>
+                </div>
+
+                <div class="stage-temoignage">
+                    <h3>Témoigne</h3>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+                </div>
+            </div> 
+    </div>
+
+    <!-- <div class="stagecontent"> -->
         <!-- LAST CARROUSEL 
          <div class="CarrouselContent">
             <div class="SliderCarrousel">
@@ -12,8 +78,6 @@
             </div>
         </div>
         -->
-
-       
         <!-- PARTAINERS
             <h4 class="nomEntreprise">Liste de partenaires </h4>
         <div class="liste">
@@ -47,7 +111,6 @@
             </ul>
         </div>
         -->
-
        <!--
         <div class="petitmot">
             <p>Stages au milieu de travail</p>
@@ -82,6 +145,6 @@
             </div> 
         </div>
         -->
-    </div>
+   <!--  </div> -->
 </section>
 <!-- Fin de la section Stage -->
