fb23cfa7f7946837534f4995652a06bb854e551c
alejandra miranda <ale.miranda890@gmail.com>
2020-12-02 15:28:05 -0500

    ajout du contenu stages avec Json

--- inclusion/sectionStage.php
+++ inclusion/sectionStage.php
@@ -24,7 +24,6 @@
         <a class="next" onclick="plusSlides(1)">&#10095;</a>
     </div>
     <br>
-    <!-- fin du conteneur carrousel -->
 
     <!-- les petits circles -->
     <div style="text-align:center">
@@ -32,119 +31,47 @@
         <span class="dot" onclick="currentSlide(2)"></span>
         <span class="dot" onclick="currentSlide(3)"></span>
     </div>
-    <!---------------------------------------------------------Fin carrousel -->
 
-    <!-------------------------------------------------------Expériences de stage -->
-    <div class="petitmot">
-        <p>stage en milieu de travail</p>
-        <hr>
-        <p>Expériences</p>
-    </div>
 
-    <div class="stagierContent">
-            <div class="stagier-info">
-                <div class="stagier">
-                    <img class="stagierPhoto" src="images/Stagiers/avatar1.JPG" alt="photo du stagier">
-                    <p>Nom étudiante</p>
-                    <p>Type de stage</p>
-                </div>
-                <div class="stage-temoignage">
-                    <h3>Témoigne</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
-                </div>  
-            </div>
-           
-            <div class="stagier-info">
-                <div class="stagier">
-                    <img class="stagierPhoto" src="images/Stagiers/avatar2.JPG" alt="photo du stagier">
-                    <p>Nom étudiant</p>
-                    <p>Type de stage</p>
-                </div>
-
-                <div class="stage-temoignage">
-                    <h3>Témoigne</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
-                </div>
-            </div> 
-    </div>
-
-    <!-- <div class="stagecontent"> -->
-        <!-- LAST CARROUSEL 
-         <div class="CarrouselContent">
-            <div class="SliderCarrousel">
-                <div class="slide photo1"></div>
-                <div class="slide photo2"></div>
-                <div class="slide photo3"></div>
-            </div>
-        </div>
-        -->
-        <!-- PARTAINERS
-            <h4 class="nomEntreprise">Liste de partenaires </h4>
-        <div class="liste">
-            <ul class="items">
-                <li><a class="links" href="https://sidlee.com/en">SidLee</a></li>
-                <li><a class="links" href="https://www.adviz.ca/">Adviz Media inc</a></li>
-                <li><a class="links" href="https://www.affairemedia.com/">Affaire Média</a></li>
-                <li><a class="links" href="https://www.agencearchipel.com/en/">Agence archipel</a></li>
-                <li><a class="links" href="https://consultantmarketingcfl.ca/">Agence minimale</a></li>
-            </ul>
-            <ul class="items">
-                <li><a class="links" href="https://agence.ninja/">Agence ninja</a></li>
-                <li><a class="links" href="http://www.agencetopo.qc.ca/wp/">Agence Topo</a></li>
-                <li><a class="links" href="https://annx.digital/">Annexe<a></li>
-                <li><a class="links" href="http://www.appwapp.com/en/">appwap</a></li>
-                <li><a class="links" href="https://www.aptitudex.com/">aptitude x</a></li>
-            </ul>
-            <ul class="items">
-                <li><a class="links" href="https://sidlee.com/en">SidLee</a></li>
-                <li><a class="links" href="https://www.adviz.ca/">Adviz Media inc</a></li>
-                <li><a class="links" href="https://www.affairemedia.com/">Affaire Média</a></li>
-                <li><a class="links" href="https://www.agencearchipel.com/en/">Agence archipel</a></li>
-                <li><a class="links" href="https://consultantmarketingcfl.ca/">Agence minimale</a></li>
-            </ul>
-            <ul class="items">
-                <li><a class="links" href="https://agence.ninja/">Agence ninja</a></li>
-                <li><a class="links" href="http://www.agencetopo.qc.ca/wp/">Agence Topo</a></li>
-                <li><a class="links" href="https://annx.digital/">Annexe<a></li>
-                <li><a class="links" href="http://www.appwapp.com/en/">appwap</a></li>
-                <li><a class="links" href="https://www.aptitudex.com/">aptitude x</a></li>
-            </ul>
-        </div>
-        -->
-       <!--
-        <div class="petitmot">
-            <p>Stages au milieu de travail</p>
+<!-------------------------------------------------------Expériences de stage -->
+<div class="petitmot">
+            <p>stage en milieu de travail</p>
             <hr>
             <p>Expériences</p>
         </div>
-        <div class="stagierContent">
-                
-            <div class="stagier-info">
-                <div class="stagier">
-                    <img class="stagierPhoto" src="images/Stagiers/avatar1.JPG" alt="photo du stagier">
-                    <p>Nom étudiante</p>
-                    <p>Type de stage</p>
-                </div>
-                <div class="stage-temoignage">
-                    <h3>Témoigne</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
-                </div>  
-            </div>
-           
-            <div class="stagier-info">
-                <div class="stagier">
-                    <img class="stagierPhoto" src="images/Stagiers/avatar2.JPG" alt="photo du stagier">
-                    <p>Nom étudiant</p>
-                    <p>Type de stage</p>
-                </div>
 
-                <div class="stage-temoignage">
-                    <h3>Témoigne</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+<!------------------------------------------------------ Json -------- -->
+<?php
+    // Lire le fichier JSON dans une chaîne de caractères
+    $stageExpeJSONString = file_get_contents("data/stageExperiences.json");
+
+    // Décoder la chaîne JSON dans une structure de données PHP
+    $experienceJSON = json_decode($stageExpeJSONString, true);
+
+    // Parcourir le tableau associatif imagesJSON
+    foreach ($experienceJSON as $experience => $uneExperience) :
+        foreach ($uneExperience as $unePersonne) :
+?>
+
+       
+        <div class="stagierContent">
+                <div class="stagier-info">
+                    <div class="stagier">
+                        <img src="images/Stagiers/<?= $unePersonne["image"]; ?>" class="stagierPhoto" alt="photo du stagier">
+                        <p><?= $unePersonne["nom"]; ?></p>
+                        <p><?= $unePersonne["typeStage"]; ?></p>
+                    </div>
+                    <div class="stage-temoignage">
+                        <h3>Témoigne</h3>
+                        <p><?= $unePersonne["temoignage"]; ?></p>
+                    </div>  
                 </div>
-            </div> 
-        </div>
-        -->
-   <!--  </div> -->
+        <?php
+        endforeach;
+        endforeach;
+        ?>
+
+    </div>
+    
 </section>
 <!-- Fin de la section Stage -->
