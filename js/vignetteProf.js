9a8a862bf9fcdaa3941468fec5f70b75d8f33aac
Bernard Dwyer-Beaudet <bernarddwyerb@gmail.com>
2020-12-12 17:07:17 -0500

    commentaires entete et js

--- js/vignetteProf.js
+++ js/vignetteProf.js
@@ -1,35 +1,38 @@
 
-let choixDuProf = null;
+let choixDuProf = null; // au debut pas de prof choisi 
 
-const listeProf = document.querySelectorAll(".choix-prof");
-const listeProfafficher = document.querySelectorAll(".afficher-prof");
+const listeProf = document.querySelectorAll(".choix-prof"); //conserver le id du choix de prof 
+const listeProfafficher = document.querySelectorAll(".afficher-prof"); // passer le id du prof a afficher 
+/*
+function permet de choisir un nom de professuer et faire appatraitre les infos du professeur choisi
 
-//document.addEventListener('onClick', choixProf);      
+
+*/
+    
 for (const leProf of listeProf) {
     leProf.addEventListener("click", function(){
-       // console.log("ok");
+    
         afficherUnProf(this);
     });
 }
 
-
+    // mettre le id du professeur pour pouvoir le choisir 
  function afficherUnProf(elmProf){
     let idProf = elmProf.getAttribute("data-prof");
     let detailProf = document.getElementById("prof" + idProf);
     
-    //console.log("Allo");
     // Fermer la vignette du professeur 
 for (const unProf of listeProfafficher) {
 
         unProf.style.display = "flex";
-        
     }
 
+    // pour choisir un prof different
 for (const unProf of listeProfafficher) {
     if (unProf.id != "prof"+idProf && unProf.id != "") {
 
-        console.log(elmProf.id, "elmprof");
-        console.log(unProf.id, "unprof");
+        // console.log(elmProf.id, "elmprof");
+        // console.log(unProf.id, "unprof");
 
         unProf.style.display = "none";
         elmProf.style.color = "darkgray";
