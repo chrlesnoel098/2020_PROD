279071b56b1b37663e0c0c6612dcc9a7ca8d42af
Charles Noël <charlesnoel098@gmail.com>
2020-12-14 17:10:05 -0500

    Commentaire php JSON SASS et JS

--- inclusion/header.php
+++ inclusion/header.php
@@ -2,99 +2,112 @@
 <html lang="fr">
 
 <head>
+    <!-- metadata -->
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TIM</title>
     <meta name="description" content="TIM - Techinque d'Intégration multimédia collège Maisonneuve">
+    <meta name="keywords" content="TIM, Techinque, d'Intégration, multimédia, collège, Maisonneuve">
+    <meta name="author" content="Charles Noël, Bernard Dwyer, Alejandra Miranda, Lauryan Gervais">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- titre du site -->
+    <title>TIM</title>
+    <!-- favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.png">
-    <link rel="mask-icon" href="images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicons/favicon-16x16.png">
+    <link rel="mask-icon" href="./images/favicons/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#000000">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/all.min.css">
-    <link rel="stylesheet" href="css/fontawesome.min.css">
-    <link rel="stylesheet" href="css/styles.css">
-    
+    <!-- font globale -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- feuille de style css -->
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/all.min.css">
+    <link rel="stylesheet" href="./css/fontawesome.min.css">
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
+
 <body>
-<div id="background-noise">
-    <!-- Début header -->
-    <header id="masthead" class="site-header">
-        <div class="site-branding">
-            <h1 class="site-title"><a url="#"></a></h1>
-            <div id="logo-cercle"></div>
-        </div>
-    <!-- Navigation bureau -->
-        <nav id="site-navigation" class="main-navigation">
-        <div class="progress"></div>
-            <div class="menu-menu-1-container">
-                <ul id="primary-menu" class="menu nav-menu">
-     
-                    <li><a class="links" href="#" aria-current="page"><div class="progress"></div><div class="cube-wrap"><div class="cube"><div class="side top"></div><div class="side bottom"></div><div class="side front"></div><div class="side back"></div><div class="side left"></div><div class="side right"></div></div></div></a></li>
-                    <li><a class="links" href="#SectionProgramme">Programme</a></li>
-                    <li><a class="links" href="#SectionProfesseur">Professeur</a></li>
-                    <li><a class="links" href="#SectionProjet">Étudiant</a></li>
-                    <li><a class="links" href="#SectionStage">Stage</a></li>
-                    <li><a class="links" href="#SectionEvenement">Évènement</a></li>
-                    <li><a target="_blank" href="https://www.facebook.com/maisonneuvetim/"><img
-                                src="images/icone-facebook.png" alt=""></a></li>
-                    <li><a target="_blank" href="https://www.instagram.com/maisonneuvetim/"><img
-                                src="images/icone-instagram.png" alt=""></a></li>
-                    <li><a target="_blank" href="https://www.youtube.com/user/TIMaisonneuve"><img
-                                src="images/icone-youtube.png" alt=""></a></li>
-                    <li><a target="_blank" href="https://discord.com/invite/53vFvr9"><img src="images/icone-discord.png"
-                                alt=""></a></li>
-                </ul>
+    <div id="background-noise">
+        <!-- Début header -->
+        <header id="masthead" class="site-header">
+            <div class="site-branding">
+                <h1 class="site-title"><a url="#"></a></h1>
+                <div id="logo-cercle"></div>
             </div>
-        </nav>
+            <!-- Navigation bureau -->
+            <nav id="site-navigation" class="main-navigation">
+                <div class="progress"></div>
+                <div class="menu-menu-1-container">
+                    <ul id="primary-menu" class="menu nav-menu">
+
+                        <li><a class="links" href="#" aria-current="page">
+                                <div class="progress"></div>
+                                <div class="cube-wrap">
+                                    <div class="cube">
+                                        <div class="side top"></div>
+                                        <div class="side bottom"></div>
+                                        <div class="side front"></div>
+                                        <div class="side back"></div>
+                                        <div class="side left"></div>
+                                        <div class="side right"></div>
+                                    </div>
+                                </div>
+                            </a></li>
+                        <li><a class="links" href="#SectionProgramme">Programme</a></li>
+                        <li><a class="links" href="#SectionProfesseur">Professeur</a></li>
+                        <li><a class="links" href="#SectionProjet">Étudiant</a></li>
+                        <li><a class="links" href="#SectionStage">Stage</a></li>
+                        <li><a class="links" href="#SectionEvenement">Évènement</a></li>
+                        <li><a target="_blank" href="https://www.facebook.com/maisonneuvetim/"><img src="./images/icone-facebook.png" alt=""></a></li>
+                        <li><a target="_blank" href="https://www.instagram.com/maisonneuvetim/"><img src="./images/icone-instagram.png" alt=""></a></li>
+                        <li><a target="_blank" href="https://www.youtube.com/user/TIMaisonneuve"><img src="./images/icone-youtube.png" alt=""></a></li>
+                        <li><a target="_blank" href="https://discord.com/invite/53vFvr9"><img src="./images/icone-discord.png" alt=""></a></li>
+                    </ul>
+                </div>
+            </nav>
 
 
-        <!-- burger Menu -->
-        <div class="menu-bar-movil">
-            <a href="#" class="logo">
-                <img src="images/LogoTIM.png" alt="Logo du DEC Technique d'intégration multimédia">
-            </a>
+            <!-- burger Menu -->
+            <div class="menu-bar-movil">
+                <a href="#" class="logo">
+                    <img src="images/LogoTIM.png" alt="Logo du DEC Technique d'intégration multimédia">
+                </a>
 
-            <div class="technique">
-                <p>TECHNIQUE D'INTÉGRATION MULTIMÉDIA</p>
-            </div>
-        
-            <div class="burger-menu" id="burger_menu">
-                <i class="fas fa-bars" aria-expanded="false"></i>
-            </div>
-            
-            <div class="slideMenu" id="slideMenu">
-                <div class="top-redes">
-                    <a target="_blank" href="https://www.facebook.com/maisonneuvetim/"><img src="images/icone-facebook.png" alt=""></a>
-                    <a target="_blank" href="https://www.instagram.com/maisonneuvetim/"><img src="images/icone-instagram.png" alt=""></a>   
-                    <a target="_blank" href="https://www.youtube.com/user/TIMaisonneuve"><img src="images/icone-youtube.png" alt=""></a>
-                    <a target="_blank" href="https://discord.com/invite/53vFvr9"><img src="images/icone-discord.png" alt=""></a>
+                <div class="technique">
+                    <p>TECHNIQUE D'INTÉGRATION MULTIMÉDIA</p>
                 </div>
 
-                <nav class="menu-principal">
-                    <a href="#">Accueil</a>
-                    <a href="#SectionProgramme">Programme</a>
-                    <a href="#SectionProfesseur">Professeur</a>
-                    <a href="#SectionProjet">Étudiant</a>
-                    <a href="#SectionStage">Stage</a>
-                    <a href="#SectionEvenement">Évènement</a>
-                </nav>
+                <div class="burger-menu" id="burger_menu">
+                    <i class="fas fa-bars" aria-expanded="false"></i>
+                </div>
 
-                <!-- <div class="fermer" id="fermer">
+                <div class="slideMenu" id="slideMenu">
+                    <div class="top-redes">
+                        <a target="_blank" href="https://www.facebook.com/maisonneuvetim/"><img src="./images/icone-facebook.png" alt=""></a>
+                        <a target="_blank" href="https://www.instagram.com/maisonneuvetim/"><img src="./images/icone-instagram.png" alt=""></a>
+                        <a target="_blank" href="https://www.youtube.com/user/TIMaisonneuve"><img src="./images/icone-youtube.png" alt=""></a>
+                        <a target="_blank" href="https://discord.com/invite/53vFvr9"><img src="./images/icone-discord.png" alt=""></a>
+                    </div>
+
+                    <nav class="menu-principal">
+                        <a href="#">Accueil</a>
+                        <a href="#SectionProgramme">Programme</a>
+                        <a href="#SectionProfesseur">Professeur</a>
+                        <a href="#SectionProjet">Étudiant</a>
+                        <a href="#SectionStage">Stage</a>
+                        <a href="#SectionEvenement">Évènement</a>
+                    </nav>
+
+                    <!-- <div class="fermer" id="fermer">
                     <i class="fas fa-times"></i>
                 </div> -->
+                </div>
             </div>
-        </div>
 
 
-    </header>
-    <!-- Fin header -->
+        </header>
+        <!-- Fin header -->
 
 
-    <div class="site-main">
-        <!-- Début de la section Stage -->
+        <div class="site-main">
+            <!-- Début de la section Stage -->
--- js/ScrollAnim.js
+++ js/ScrollAnim.js
@@ -1,6 +1,43 @@
+/**
+ * 
+ * Fichier ScrollAnim.js
+ * 
+ * Auteur: Charles Noël
+ * 
+ * Description: Fichier qui gère les ancrages de la navigation 
+ * et correction de la vitesse de navigation
+ * 
+ * Event : L'évènement scroll calcul de valeur en Y + appel Throttle,
+ * 
+ */
+
+
+
+/**
+ * Description: L'évènement scroll qui calcul la valeur en Y et l'associe au style body
+ *  
+ * @param {event} changerEtatLiens L'évènement scroll calcul de valeur en Y
+ * @param {return} false return false
+ * 
+ * Style : Body 
+ * 
+ */
+
+// appelation de l'évènement scroll
 window.addEventListener('scroll', () => {
+  // association de la propriété scroll Y en valeur au Body, ajustement fait par css avec l'appelation --scroll
   document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
+
+
+/**
+ * 
+ * Description: La limitation et l'anti-rebond nous permettent de contrôler la vitesse à laquelle une fonction est appelée.
+ * 
+ * 
+ * @see https://codeburst.io/throttling-and-debouncing-in-javascript-b01cad5c8edf Référence utilisée.
+ * 
+ */
 
 // ajustement du scroll, simple correction throttle.
 const throttle = (func, limit) => {
@@ -16,28 +53,39 @@ const throttle = (func, limit) => {
   }
 }
 
-// ce qui fait fonctionner tes references
 
-const links = document.querySelectorAll('.links'); // la class a cibler pour ajouter active apres.
+/**
+
+  * Description: Faire basculer la class selon la section, le titre des sections pour déclencher les animations en css
+  *
+  * @param {function} changerEtatLiens Change l'état des liens en basculer la class .liensActif ou .bgBlue
+  * @param {const} liens class ciblé des liens de la navigation
+  * @param {const} h1s class ciblé des titres des sections
+  * @param {const} sections class ciblé des sections 
+  * 
+  * @param {return} default return default
+  * 
+  */
+
+// ce qui fait fonctionner les references
+const liens = document.querySelectorAll('.links'); // la class a cibler pour ajouter active apres.
 const h1s = document.querySelectorAll('h1'); // la position qui s`index.
 const sections = document.querySelectorAll('header>h1, section#Programme, section#Professeur, section#Projet, section#Stage, section#Evenement'); // la position qui s`index.
 
-function changeLinkState() {
-  let indexLink = h1s.length;
-  let indexSection = sections.length;
+function changerEtatLiens() {
+  let indexLink = h1s.length; // nombre de liens
+  let indexSection = sections.length; // nombre de sections
 
-  while (--indexLink && window.scrollY + 50 < h1s[indexLink].offsetTop) {}
+  while (--indexLink && window.scrollY + 50 < h1s[indexLink].offsetTop) { }
 
-  links.forEach((link) => link.classList.remove('liensActif')); // remove .active
-  links[indexLink].classList.add('liensActif'); // ajout .active
+  liens.forEach((link) => link.classList.remove('liensActif')); // remove .active
+  liens[indexLink].classList.add('liensActif'); // ajout .active
 
-  while (--indexSection && window.scrollY + 50 < sections[indexSection].offsetTop) {}
+  while (--indexSection && window.scrollY + 50 < sections[indexSection].offsetTop) { }
 
   sections.forEach((section) => section.classList.remove('bgBlue')); // remove .active
   sections[indexSection].classList.add('bgBlue'); // ajout .active
-
-
 }
 
-changeLinkState(); // appel fonction
-window.addEventListener('scroll', throttle(changeLinkState), 1000); // appel event
+changerEtatLiens(); // appel fonction
+window.addEventListener('scroll', throttle(changerEtatLiens), 1000); // appel de l'évènement scroll avec le throttle* sur la fonction changerEtatLiens avec un delai
